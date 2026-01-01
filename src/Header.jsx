792808c89a568182783d21dab149eb2f0a6fbfbe
Rafaela Sousa Costa <rafaelaengsousa@gmail.com>
2026-01-01 11:24:12 -0300

    criando header

---
+++ src/Header.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function Header() {
+  return (
+    <header className= "flex justify-center py-9 bg-amber-100">
+      <div className>
+        <h1 className=" text-4xl text-rose-500  align-middle">
+          Lista de Tarefas
+        </h1>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
